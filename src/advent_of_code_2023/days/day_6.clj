d47925cf8dbc22f69fda9bae7d8b1ea75600021e
fabriciorby <fabriciorby@hotmail.com>
2023-12-06 18:57:37 +0100

    day 6 - not optimized

---
+++ src/advent_of_code_2023/days/day_6.clj
@@ -0,0 +1,34 @@
+(ns advent-of-code-2023.days.day-6
+  (:require [advent-of-code-2023.core :as helper]
+            [clojure.string :as str]))
+
+(defn parse [lines]
+  (letfn [(get-numbers [line] (map #(Integer/parseInt %) (rest (str/split line #"\s+"))))]
+    {
+     :times     (get-numbers (first lines))
+     :distances (get-numbers (second lines))
+     }
+    )
+  )
+
+(defn parse-2 [lines]
+  (letfn [(get-numbers [line] (Long/parseLong (str/join (rest (str/split line #"\s+")))))]
+    {
+     :time     (get-numbers (first lines))
+     :distance (get-numbers (second lines))
+     }
+    )
+  )
+
+(defn calculate [time distance]
+  (count (filter pos-int? (map - (map-indexed * (range time 0 -1)) (repeat time distance))))
+  )
+
+(defn -main []
+  (let [lines (helper/get-lines "day-6.txt")
+        parsed-input (parse lines)
+        parsed-input-2 (parse-2 lines)]
+    (reduce * (map calculate (:times parsed-input) (:distances parsed-input)))
+    (calculate (:time parsed-input-2) (:distance parsed-input-2))
+    )
+  )
