4c72af08bbd50511eb74e46cc9ec7ccc399ea3cc
fabriciorby <fabriciorby@hotmail.com>
2023-12-16 16:42:47 +0100

    part 2 - never finished

--- src/advent_of_code_2023/days/day_12.clj
+++ src/advent_of_code_2023/days/day_12.clj
@@ -10,6 +10,7 @@
 
 (defn all-possibilities
   ([springs]
+   (println "All possibilities for " springs)
    (map str/join (partition (count springs) (all-possibilities springs []))))
   ([[spring & remaining] acc]
    (if (nil? spring)
@@ -18,21 +19,31 @@
        (flatten (vector
                   (all-possibilities remaining (conj acc \.))
                   (all-possibilities remaining (conj acc \#))))
-       (vector (all-possibilities remaining (conj acc spring)))
+       (flatten (vector (all-possibilities remaining (conj acc spring))))
        )
      ))
   )
 
-(defn valid [possibility separations]
-
-  ;; check with regex (no internet) :'(
-
+(defn is-valid? [possibility separations]
+  (let [regex
+        (re-pattern
+                (str "(\\.+)?" (str/join "(\\.+)" (map #(str "(#){" % "}") separations)) "(\\.+)?")
+                )]
+    (if-let [[result] (re-matches regex possibility)]
+      (if (= result possibility) true false)
+      false
+      )
+    )
+  )
+(defn valid-possibilities [possibilities separation]
+  (count (filter true? (map #(is-valid? % separation) possibilities)))
   )
 
 (defn -main []
-  (let [input (get-lines "day-12-example.txt")
-        parsed (mapv parse input)]
-    ;(map all-possibilities parsed)
-    (all-possibilities (first (last parsed)))
+  (let [input (get-lines "day-12.txt")
+        parsed (mapv parse input)
+        possibilities (map first parsed)
+        separations (map second parsed)]
+    (println (reduce + (map valid-possibilities (map all-possibilities parsed) separations)))
     )
   )
