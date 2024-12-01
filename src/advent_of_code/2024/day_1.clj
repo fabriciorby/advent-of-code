d90e8a88ae07b75cf567b6333258789963a9211b
fabriciorby <fabriciorby@hotmail.com>
2024-12-01 21:42:00 +0100

    day 1 - 2024

---
+++ src/advent_of_code/2024/day_1.clj
@@ -0,0 +1,17 @@
+(ns advent-of-code.2024.day-1
+  (:require [advent-of-code.core :as helper]
+            [clojure.string :as str]))
+
+(def lines (helper/get-lines "2024/day-1.txt"))
+(defn parse-to-int-pair [line] (map read-string (str/split line #"   ")))
+(defn parse [lines] (->> (map parse-to-int-pair lines) (apply map list)))
+(defn get-difference [lines] (->> (apply map - lines) (map abs)))
+(defn get-similarity-score [[l1 l2]] (map #(* % (get (frequencies l2) % 0)) l1))
+
+(defn -main []
+  (->> (parse lines) (map sort)
+       (get-difference) (reduce +)
+       (println))
+  (->> (parse lines)
+       (get-similarity-score) (reduce +)
+       (println)))
