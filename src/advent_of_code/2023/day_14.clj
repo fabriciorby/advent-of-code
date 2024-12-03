e09bf3962b55fe84de2936bb07f30c53fea5fad0
fabriciorby <fabriciorby@hotmail.com>
2024-12-03 21:02:37 +0100

    cleanup

--- src/advent_of_code/2023/day_14.clj
+++
@@ -1,7 +0,0 @@
-(ns advent-of-code.2023.day-14
-  (:use [advent-of-code.utils]))
-
-(defn -main []
-  (let [input (get-lines "2023/day-14-example.txt")]
-    )
-  )
--- src/advent_of_code/2023/day_15.clj
+++
@@ -1,7 +0,0 @@
-(ns advent-of-code.2023.day-15
-  (:use [advent-of-code.utils]))
-
-(defn -main []
-  (let [input (get-lines "2023/day-15-example.txt")]
-    )
-  )
